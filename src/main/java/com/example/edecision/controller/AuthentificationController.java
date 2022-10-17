56c240bfa9b847947eafad02eaab1bba678c61ac
Propriétaire <jayetlucas@gmail.com>
2022-10-17 15:25:43 +0200

    rename folders

--- src/main/java/com/example/edecision/controller/AuthentificationController.java
+++ src/main/java/com/example/edecision/controller/AuthentificationController.java
@@ -1,4 +1,4 @@
-package Controller;
+package com.example.edecision.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
--- src/main/java/com/example/edecision/model/Utilisateur.java
+++ src/main/java/com/example/edecision/model/Utilisateur.java
@@ -1,4 +1,4 @@
-package Model;
+package com.example.edecision.model;
 
 public class Utilisateur {
 	private int id;
