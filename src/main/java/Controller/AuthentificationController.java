0b964e092d69bba6f58a5ae7b43e24da47c73b98
Propriétaire <jayetlucas@gmail.com>
2022-10-17 14:49:01 +0200

    create folders

---
+++ src/main/java/Controller/AuthentificationController.java
@@ -0,0 +1,12 @@
+package Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthentificationController {
+	@RequestMapping({ "/hello" })
+	public String firstPage() {
+		return "Hello World";
+	}
+}
